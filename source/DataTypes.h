eb3cb046e9c8d005c56344315e30f4abfee584e3
Jolan Plaum <jolan.plaum@student.howest.be>
2023-01-22 20:55:40 +0100

    Fixed Vertex

--- source/DataTypes.h
+++ source/DataTypes.h
@@ -5,17 +5,17 @@ namespace dae
 	struct Vertex
 	{
 		Vector3 position{};
-		Vector2 uv{};
 		Vector3 normal{};
 		Vector3 tangent{};
+		Vector2 uv{};
 	};
 
 	struct Vertex_Out
 	{
 		Vector4 position{};
-		Vector2 uv{};
 		Vector3 normal{};
 		Vector3 tangent{};
+		Vector2 uv{};
 		Vector3 worldPosition{};
 	};
 }
